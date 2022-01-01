89537f69125dfc8dbd4907f7210e52891be6f40f
Cody Brunner <me@codybrunner.com>
2022-01-01 12:00:43 -0500

    fix(components): tries a different loader config for images

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react'
 import NextImage, { ImageProps, ImageLoader } from 'next/image'
 
-const cloudflareImageLoader: ImageLoader = ({ src, width, quality }) => {
-  return `${process.env.IMAGE_WORKER_URL}?width=${width}&quality=${
-    quality ?? 75
-  }&image=https://codybrunner.com${src}`
+const normalizeSrc = (src: string) => {
+  return src[0] === '/' ? src.slice(1) : src
 }
+
+const cloudflareImageLoader: ImageLoader = ({ src, width, quality }) => {
+  const params = [`width=${width}`]
+  if (quality) {
+    params.push(`quality=${quality}`)
+  }
+  const paramsString = params.join(',')
+  return `/cdn-cgi/image/${paramsString}/${normalizeSrc(src)}`
+}
+
+// const cloudflareImageLoader: ImageLoader = ({ src, width, quality }) => {
+//   return `${process.env.IMAGE_WORKER_URL}?width=${width}&quality=${
+//     quality ?? 75
+//   }&image=https://codybrunner.com${src}`
+// }
 
 const svgLoader: ImageLoader = ({ src }) => src
 
