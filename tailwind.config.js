e4ca537dbbec8c297ce5ff7fcdc2a598f67b5a3b
Cody Brunner <me@codybrunner.com>
2022-09-11 16:39:48 -0500

    feat(tailwind): adds theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { fontFamily } = require('tailwindcss/defaultTheme')
+const { black, blue, red, white, yellow } = require('tailwindcss/colors')
+
+const aura = {
+	black: '#15141b',
+	blue: '#82e2ff',
+	gray: '#6d6d6d',
+	green: '#61ffca',
+	orange: '#ffca85',
+	pink: '#f694ff',
+	purple: '#a277ff',
+	'purple-fading': '#3d375e7f',
+	red: '#ff6767',
+	white: '#edecee',
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,md,mdx,ts,tsx}'],
-	theme: {},
+	theme: {
+		colors: {
+			black,
+			blue: blue['500'],
+			current: 'currentColor',
+			transparent: 'transparent',
+			aura: { ...aura },
+			red: red['500'],
+			white,
+			yellow: yellow['500'],
+		},
+		fontFamily: {
+			...fontFamily,
+		},
+	},
 	plugins: [],
 	variants: {},
 }
