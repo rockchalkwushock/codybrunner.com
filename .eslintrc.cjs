47587e8b2836bf8adac1b98422308b40e46d1be0
Cody Brunner <me@codybrunner.com>
2022-09-11 15:19:39 -0500

    feat(devtools): sets up devtools

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:astro/recommended',
+		'plugin:jsx-a11y/strict',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:tailwindcss/recommended',
+	],
+	overrides: [
+		{
+			files: ['*.astro'],
+			parser: 'astro-eslint-parser',
+			parserOptions: {
+				extraFileExtensions: ['.astro'],
+				parser: '@typescript-eslint/parser',
+			},
+			rules: {},
+		},
+	],
+	root: true,
+	rules: {},
+}
---
+++ postcss.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+	plugins: {
+		'tailwindcss/nesting': {},
+		tailwindcss: {},
+		'postcss-preset-env': {},
+		autoprefixer: {},
+		cssnano: {
+			preset: 'default',
+		},
+	},
+}
