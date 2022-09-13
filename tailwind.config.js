9c4ffed5024a0ca2a07bbbc5b73e95da66e36d0e
Cody Brunner <me@codybrunner.com>
2022-09-13 11:01:20 -0500

    chore(root): removes Socials

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,37 +19,18 @@ const aura = {
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,md,mdx,ts,tsx}'],
 	theme: {
-		animation: {
-			// utility: animation-wiggle
-			wiggle: 'wiggle 1s ease-in-out infinite',
-		},
-		backgroundImage: {
-			// bg-instagram
-			instagram:
-				'radial-gradient(circle at 30% 107%, #fdf497 0%,#fdf497 5%,#fd5949 45%,#d6249f 60%,#285aeb 90%)',
-		},
 		colors: {
 			aura: { ...aura },
 			black,
 			blue: blue['500'],
 			current: 'currentColor',
-			// bg-linkedin || text-linkedin
-			linkedin: '#0072B1',
 			red: red['500'],
 			transparent: 'transparent',
-			// bg-twitter || text-twitter
-			twitter: '#1DA1F2',
 			white,
 			yellow: yellow['500'],
 		},
 		fontFamily: {
 			...fontFamily,
-		},
-		keyframes: {
-			wiggle: {
-				'0% 100%': { transform: 'rotate(-12deg) scale(0.95)' },
-				'50%': { transform: 'rotate(12deg) scale(0.95)' },
-			},
 		},
 	},
 	plugins: [],
