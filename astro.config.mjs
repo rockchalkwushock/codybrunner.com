63f5df94d955682e0166df697465b984bf5ef411
Cody Brunner <me@codybrunner.com>
2022-09-17 18:40:14 -0500

    feat(root): adds sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,29 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind' // Markdown & MDX Plugins
 
-// Markdown & MDX Plugins
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeExternalLinks from 'rehype-external-links'
 import rehypeSlug from 'rehype-slug'
 import remarkCodeTitles from 'remark-code-titles'
-import remarkValidateLinks from 'remark-validate-links'
-// User-Rolled Plugins
+import remarkValidateLinks from 'remark-validate-links' // User-Rolled Plugins
+
 import { remarkReadingTime } from './src/utils/remark-reading-time'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), tailwind({ config: { applyBaseStyles: false } })],
+	integrations: [
+		mdx(),
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		// https://docs.astro.build/en/guides/integrations-guide/sitemap/
+		sitemap(),
+	],
 	markdown: {
 		extendDefaultPlugins: true,
 		rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, rehypeExternalLinks],
