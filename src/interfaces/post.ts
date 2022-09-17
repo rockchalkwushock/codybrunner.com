5dfa1292d37042a976b15cd35726c8ebb8648842
Cody Brunner <me@codybrunner.com>
2022-09-16 20:41:12 -0500

    refactor(interfaces): updates types

--- src/interfaces/post.ts
+++ src/interfaces/post.ts
@@ -1,59 +1,70 @@
 import type { MDXInstance } from 'astro'
 
-export type Category = 'business' | 'food' | 'life' | 'technology' | 'travel'
+export const CATEGORIES = [
+	'business',
+	'food',
+	'life',
+	'technology',
+	'travel',
+] as const
+export type CategoryTuple = typeof CATEGORIES
+export type Category = CategoryTuple[number]
 
-export type Tag =
-	| 'absinthe'
-	| 'asd'
-	| 'astrojs'
-	| 'autism'
-	| 'aws'
-	| 'cli'
-	| 'cloudflare'
-	| 'colombia'
-	| 'django'
-	| 'docker'
-	| 'docker-compose'
-	| 'docker-swarm'
-	| 'ecto'
-	| 'elixir'
-	| 'erlang'
-	| 'expat'
-	| 'fly'
-	| 'git'
-	| 'golang'
-	| 'graphql'
-	| 'hasura'
-	| 'hugo'
-	| 'immigration'
-	| 'iterm'
-	| 'javascript'
-	| 'k3s'
-	| 'k8s'
-	| 'linux'
-	| 'liveview'
-	| 'macos'
-	| 'nextjs'
-	| 'netlify'
-	| 'nodejs'
-	| 'opinion'
-	| 'oss'
-	| 'personal'
-	| 'phoenix'
-	| 'postgresql'
-	| 'python'
-	| 'raspberry-pi'
-	| 'reactjs'
-	| 'react-query'
-	| 'rustlang'
-	| 'sql'
-	| 'ssh'
-	| 'svelte'
-	| 'tailscale'
-	| 'tailwindcss'
-	| 'typescript'
-	| 'vercel'
-	| 'vscode'
+export const TAGS = [
+	'absinthe',
+	'asd',
+	'astrojs',
+	'autism',
+	'aws',
+	'cli',
+	'cloudflare',
+	'colombia',
+	'django',
+	'docker',
+	'docker-compose',
+	'docker-swarm',
+	'ecto',
+	'elixir',
+	'erlang',
+	'expat',
+	'fly',
+	'git',
+	'golang',
+	'graphql',
+	'hasura',
+	'hugo',
+	'immigration',
+	'iterm',
+	'javascript',
+	'k3s',
+	'k8s',
+	'linux',
+	'liveview',
+	'macos',
+	'nextjs',
+	'netlify',
+	'nodejs',
+	'opinion',
+	'oss',
+	'personal',
+	'phoenix',
+	'postgresql',
+	'python',
+	'raspberry-pi',
+	'reactjs',
+	'react-query',
+	'rustlang',
+	'sql',
+	'ssh',
+	'svelte',
+	'tailscale',
+	'tailwindcss',
+	'typescript',
+	'vercel',
+	'vscode',
+] as const
+export type TagTuple = typeof TAGS
+export type Tag = TagTuple[number]
 
 export interface Frontmatter {
 	author: string
