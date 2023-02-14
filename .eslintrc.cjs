1023ba4dce4f1c3bdd24c371d9c7491574d46ddf
Cody Brunner <me@codybrunner.com>
2023-02-13 22:42:00 -0500

    fix(root): fixes linting, formatting bugs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,49 +1,65 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
     'standard-with-typescript',
-		'plugin:mdx/recommended',
+    'plugin:astro/jsx-a11y-strict',
+    'plugin:mdx/recommended',
+    'plugin:astro/recommended',
     'plugin:tailwindcss/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: [
- 		'node_modules/',
- 		'dist/',
+    'node_modules/',
+    'dist/',
     'astro.config.mjs',
- 		'package.json',
- 		'tsconfig.json',
- 	],
+    'package.json',
+    'tsconfig.json',
+  ],
   overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ['*.astro'],
+      // Allows Astro components to be parsed.
+      parser: 'astro-eslint-parser',
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+      rules: {
+        'prettier/prettier': 'off',
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
- 		project: './tsconfig.json',
- 		sourceType: 'module',
+    project: './tsconfig.json',
+    sourceType: 'module',
   },
   root: true,
-  rules: {
-  },
+  rules: {},
   settings: {
- 		packageManager: 'pnpm',
- 		tailwindcss: {
- 			callees: ['classnames', 'clsx', 'ctl'],
- 			classRegex: '^class(Name)?$',
- 			config: './tailwind.config.cjs',
- 			cssFiles: [
- 				'**/*.css',
- 				'!**/node_modules',
- 				'!**/.*',
- 				'!**/dist',
- 				'!**/build',
- 			],
- 			cssFilesRefreshRate: 5_000,
- 			removeDuplicates: true,
- 			skipClassAttribute: false,
- 			tags: [],
- 			whitelist: [],
- 		},
- 	},
+    packageManager: 'pnpm',
+    tailwindcss: {
+      callees: ['classnames', 'clsx', 'ctl'],
+      classRegex: '^class(Name)?$',
+      config: './tailwind.config.cjs',
+      cssFiles: [
+        '**/*.css',
+        '!**/node_modules',
+        '!**/.*',
+        '!**/dist',
+        '!**/build',
+      ],
+      cssFilesRefreshRate: 5_000,
+      removeDuplicates: true,
+      skipClassAttribute: false,
+      tags: [],
+      whitelist: [],
+    },
+  },
 }
