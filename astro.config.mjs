647095b33e4f29a541c3599b2c431d64f1746b98
Cody Brunner <me@codybrunner.com>
2023-03-02 22:27:35 -0500

    chore(release): v1.3.0 (#365)
    * chore(pages): removes age
    
    * chore(root): removes old blog content
    
    * feat(components): adds generics
    
    * feat(components): updates Link
    
    * feat(components): updates NavItem
    
    * feat(components): updates Nav
    
    * feat(components): updates Header
    
    * feat(components): updates Footer
    
    * refactor(pages): makes use of generics
    
    * chore(components): a few more updates
    
    * chore(root): final tweaks
    
    * feat(root): adds partytown integration
    
    * feat(books): updates books
    
    * styles(layout): updates base layout
    
    * styles(components): updates footer
    
    * styles(pages): updates home page
    
    * fix(styles): fixes broken responsiveness
    
    * chore(root): comments out partytown
    
    * feat(pages): updates home page bio
    
    * feat(public): adds new resume
    
    * chore(release): v1.3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from 'astro/config'
 import image from '@astrojs/image'
-import mdx from '@astrojs/mdx'
+// import partytown from '@astrojs/partytown'
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
-		mdx(),
 		sitemap(),
 		image({ serviceEntryPoint: '@astrojs/image/sharp' }),
+		// partytown(),
 	],
+	server: {
+		open: true,
+		port: 3000,
+	},
 	site: 'https://codybrunner.com',
 })
