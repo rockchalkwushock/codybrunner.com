ea9ffc42a8ba34f979e7a292e0636d2fb214c413
Cody Brunner <me@codybrunner.com>
2023-03-05 21:42:06 -0500

    feat(root): adds uses page

---
+++ src/data/uses.ts
@@ -0,0 +1,134 @@
+interface Item {
+	name: string
+	url: string
+}
+
+export const hardware: Item[] = [
+	{
+		name: '14" MacBook Pro M1 Pro (2021)',
+		url: 'https://www.apple.com/macbook-pro/',
+	},
+	{
+		name: '32" Dell Curved 4K UHD Monitor',
+		url: 'https://a.co/d/igC03ER',
+	},
+	{
+		name: 'Autonomous AI Smart Desk 2',
+		url: 'https://www.autonomous.ai/standing-desks/smartdesk-2-home?purchase_method=1',
+	},
+	{
+		name: 'Autonomous AI Ergo Pro Chair',
+		url: 'https://www.autonomous.ai/sale/evergreen-ergochair-pro-for-sale?purchase_method=1',
+	},
+	{
+		name: 'Logitech MX Master 3',
+		url: 'https://a.co/d/j741osy',
+	},
+	{
+		name: 'Logitech Ergo K860 Split Wireless Keyboard',
+		url: 'https://a.co/d/agUkKEp',
+	},
+	{
+		name: 'Soundcore by Anker Liberty 3 Pro Headphones',
+		url: 'https://a.co/d/5Pdzk9V',
+	},
+	{
+		name: 'Samsung T5 500GB',
+		url: 'https://a.co/d/cXtfm0F',
+	},
+	{
+		name: 'Thule Crossover 2 30L Backpack',
+		url: 'https://a.co/d/fxvS6Mv',
+	},
+]
+
+export const software: Item[] = [
+	{
+		name: 'Visual Studio Code',
+		url: 'https://code.visualstudio.com/',
+	},
+	{
+		name: 'iTerm 2',
+		url: 'https://iterm2.com',
+	},
+	{
+		name: 'Oh My ZSH',
+		url: 'https://ohmyz.sh/',
+	},
+	{
+		name: 'Visual Studio Code',
+		url: 'https://code.visualstudio.com/',
+	},
+	{
+		name: 'Aura Theme',
+		url: 'https://github.com/daltonmenezes/aura-theme',
+	},
+	{
+		name: 'Codelia Ligatures Light Italic (iTerm & VSCode)',
+		url: 'https://www.myfonts.com/products/ligatures-light-italic-codelia-41078',
+	},
+	{
+		name: 'Alfred (paid)',
+		url: 'https://www.alfredapp.com/',
+	},
+	{
+		name: 'asdf-vm',
+		url: 'https://asdf-vm.com/guide/getting-started.html',
+	},
+	{
+		name: 'Bartender (paid)',
+		url: 'https://www.macbartender.com/',
+	},
+	{
+		name: 'Beekeeper Studio',
+		url: 'https://www.beekeeperstudio.io/',
+	},
+	{
+		name: 'Cryptomator',
+		url: 'https://cryptomator.org/',
+	},
+	{
+		name: 'Docker',
+		url: 'https://www.docker.com/',
+	},
+	{
+		name: 'Flow (paid)',
+		url: 'https://flowapp.info/',
+	},
+	{
+		name: 'Homebrew',
+		url: 'https://brew.sh',
+	},
+	{
+		name: 'Numi (paid)',
+		url: 'https://numi.app/',
+	},
+	{
+		name: 'Obsidian (paid)',
+		url: 'https://obsidian.md/',
+	},
+	{
+		name: 'Paste (paid)',
+		url: 'https://pasteapp.io/',
+	},
+	{
+		name: 'Signal',
+		url: 'https://signal.org/en/',
+	},
+	{
+		name: 'Spotify (paid)',
+		url: 'https://www.spotify.com/',
+	},
+	{
+		name: 'Telegram',
+		url: 'https://telegram.org/',
+	},
+	{
+		name: 'tldr',
+		url: 'https://tldr.sh/',
+	},
+	{
+		name: 'World Clock',
+		url: 'http://overdesigned.net/worldclock/',
+	},
+]
