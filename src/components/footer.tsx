5a15ac410fcf638bec15cd7479334d7e64373d54
Cody Brunner <me@codybrunner.com>
2023-06-07 09:58:58 -0500

    refactor(components): refactors footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 import { component$ } from '@builder.io/qwik'
 import { useContent, useLocation } from '@builder.io/qwik-city'
 
-import { Container } from './container'
+import { InnerContainer, OuterContainer } from './container'
 import { NavLink } from './nav-link'
 import { cx } from '~/utils/cx'
 import { isMenuItemVisible } from '~/utils/filters'
@@ -13,36 +13,44 @@ export const Footer = component$(() => {
 	const { url } = useLocation()
 	return (
 		<footer class='mt-32'>
-			<Container>
+			<OuterContainer>
 				<div class='border-t border-slate-200 dark:border-slate-700/40 pb-16 pt-10'>
-					<div class='flex flex-col items-center justify-between gap-6 sm:flex-row'>
-						<div class='font-medium grid grid-cols-3 grid-rows-2 gap-4 place-items-center text-sm md:flex md:flex-wrap md:justify-center md:gap-x-6 md:gap-y-1'>
-							{menu && menu.items
-								? menu.items.filter(isMenuItemVisible).map(({ href, text }) => (
-										<NavLink
-											class={cx(href === url.pathname ? '// TODO' : '// TODO')}
-											href={href}
-											key={`footer-link-${text}`}
-										>
-											{text}
-										</NavLink>
-										// eslint-disable-next-line no-mixed-spaces-and-tabs
-								  ))
-								: null}
+					<InnerContainer>
+						<div class='flex flex-col items-center justify-between gap-6 sm:flex-row'>
+							<div class='font-medium grid grid-cols-3 grid-rows-2 gap-4 place-items-center text-sm md:flex md:flex-wrap md:justify-center md:gap-x-6 md:gap-y-1'>
+								{menu && menu.items
+									? menu.items
+											.filter(isMenuItemVisible)
+											.map(({ href, text }) => (
+												<NavLink
+													class={cx(
+														href === url.pathname
+															? 'text-cyan-500 dark:text-cyan-400'
+															: 'hover:text-cyan-500 dark:hover:text-cyan-400'
+													)}
+													href={href}
+													key={`footer-link-${text}`}
+												>
+													{text}
+												</NavLink>
+												// eslint-disable-next-line no-mixed-spaces-and-tabs
+											))
+									: null}
+							</div>
+							<div class='flex flex-col items-center space-y-1 sm:items-end lg:flex-row lg:space-x-2 lg:space-y-0'>
+								{SITE.copyright.map((str, i) => (
+									<p
+										class='text-center text-sm sm:text-right text-slate-400 dark:text-slate-500'
+										key={`copyright--${i}`}
+									>
+										{str}
+									</p>
+								))}
+							</div>
 						</div>
-						<div class='flex flex-col items-center space-y-2 sm:items-end lg:flex-row lg:space-x-2 lg:space-y-0'>
-							{SITE.copyright.map((str, i) => (
-								<p
-									class='text-center text-sm sm:text-right text-slate-400 dark:text-slate-500'
-									key={`copyright--${i}`}
-								>
-									{str}
-								</p>
-							))}
-						</div>
-					</div>
+					</InnerContainer>
 				</div>
-			</Container>
+			</OuterContainer>
 		</footer>
 	)
 })
