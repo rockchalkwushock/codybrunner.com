79f0c07ffda8f30565c1281ad7ace4ac41b57528
Cody Brunner <me@codybrunner.com>
2023-06-07 13:58:33 -0500

    chore(src): moves jobs to separate file

---
+++ src/routes/jobs.ts
@@ -0,0 +1,67 @@
+export const jobs = [
+	{
+		company: 'Bitcoin IRA',
+		end: {
+			date: new Date(),
+			label: 'Present',
+		},
+		logo: '/icons/bitcoin-ira.svg',
+		start: new Date(2022, 9, 16),
+		title: 'Senior Frontend Developer',
+	},
+	{
+		company: 'Appointlet',
+		end: {
+			date: new Date(),
+			label: 'Present',
+		},
+		logo: '/icons/appointlet.svg',
+		start: new Date(2022, 1, 1),
+		title: 'Frontend Developer',
+	},
+	{
+		company: 'JokinglyBadTech',
+		end: {
+			date: new Date(),
+			label: 'Present',
+		},
+		logo: '/icons/jbt.svg',
+		start: new Date(2022, 1, 1),
+		title: 'Owner',
+	},
+	{
+		company: 'Weeldi',
+		end: new Date(2022, 5, 31),
+		logo: '/icons/weeldi.svg',
+		start: new Date(2022, 3, 1),
+		title: 'Fullstack Developer',
+	},
+	{
+		company: 'Arrive Logistics',
+		end: new Date(2022, 2, 1),
+		logo: '/icons/arrive-logistics.svg',
+		start: new Date(2021, 8, 15),
+		title: 'Senior Frontend Developer',
+	},
+	{
+		company: 'Lean Tech',
+		end: new Date(2022, 2, 1),
+		logo: '/icons/lean-tech.svg',
+		start: new Date(2021, 9, 16),
+		title: 'Senior Frontend Developer',
+	},
+	{
+		company: 'DM.app',
+		end: new Date(2021, 9, 1),
+		logo: '/icons/dm.svg',
+		start: new Date(2021, 8, 1),
+		title: 'Frontend Developer',
+	},
+	{
+		company: 'Appointlet',
+		end: new Date(2021, 3, 1),
+		logo: '/icons/appointlet.svg',
+		start: new Date(2018, 8, 1),
+		title: 'Web Developer',
+	},
+]
