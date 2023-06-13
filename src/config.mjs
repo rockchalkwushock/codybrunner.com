18b0d3c5abf9745d3fbafeb1fc51273504344c7a
Cody Brunner <me@codybrunner.com>
2023-06-13 18:32:29 -0500

    feat(root): adds home page, designs, assets, and content

---
+++ src/config.mjs
@@ -0,0 +1,35 @@
+export const FEATURE_FLAGS = {
+	menu: /(home)/i,
+}
+
+export const HOME = {
+	description:
+		"I’m Cody, an American software developer and entrepreneur based in Colombia. I'm a Senior Frontend Developer at Bitcoin IRA where we are building the world's first and most trusted crypto IRA platform.",
+	title: 'Software developer, veteran, and smoker of the meats.',
+}
+
+export const SITE = {
+	author: 'Cody Brunner',
+	copyright: [
+		`© 2016-${new Date().getFullYear()} Cody Brunner.`,
+		'All rights reserved.',
+	],
+	creativeCommons: 'http://creativecommons.org/licenses/by-nc-nd/4.0',
+	description: 'My stretch of pipe in the world wide inter-tubes.',
+	email: 'me@codybrunner.com',
+	lang: 'en-US',
+	origin: 'https://codybrunner.com',
+	socials: {
+		github: 'https://github.com/rockchalkwushock',
+		instagram: 'https://www.instagram.com/rockchalkwushock',
+		linkedin: 'https://www.linkedin.com/in/cody-brunner',
+		telegram: 'https://t.me/rockchalkwushock',
+		twitter: 'https://twitter.com/RockChalkDev',
+	},
+	tech: {
+		mdx: 'https://mdxjs.com',
+		qwik: 'https://qwik.builder.io',
+		tailwind: 'https://tailwindcss.com',
+	},
+	title: 'codybrunner.com',
+}
---
+++ src/root.tsx
@@ -0,0 +1,43 @@
+import { component$, useStyles$ } from '@builder.io/qwik'
+import {
+	QwikCityProvider,
+	RouterOutlet,
+	ServiceWorkerRegister,
+} from '@builder.io/qwik-city'
+import { RouterHead } from './components/router-head'
+
+import globalStyles from './styles/global.css?inline'
+import '@fontsource/poppins'
+
+// import '@fontsource/roboto'
+// import '@fontsource-variable/montserrat'
+// import '@fontsource-variable/oswald'
+// import '@fontsource-variable/raleway'
+import '@fontsource-variable/playfair-display'
+
+export default component$(() => {
+	/**
+	 * The root of a QwikCity site always start with the <QwikCityProvider> component,
+	 * immediately followed by the document's <head> and <body>.
+	 *
+	 * Dont remove the `<head>` and `<body>` elements.
+	 */
+	useStyles$(globalStyles)
+
+	return (
+		<QwikCityProvider>
+			<head>
+				<meta charSet='utf-8' />
+				<link rel='manifest' href='/manifest.json' />
+				<RouterHead />
+			</head>
+			<body
+				class='antialiased h-full w-full motion-safe:scroll-smooth flex flex-col dark:bg-primary-900 dark:text-primary-200'
+				lang='en'
+			>
+				<RouterOutlet />
+				<ServiceWorkerRegister />
+			</body>
+		</QwikCityProvider>
+	)
+})
