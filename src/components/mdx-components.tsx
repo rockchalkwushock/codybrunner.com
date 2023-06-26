e9c55d30462cc821a409acc891ed870c43bd2954
Cody Brunner <me@codybrunner.com>
2023-06-26 14:57:05 -0500

    feat(components): adds mdx components

---
+++ src/components/mdx-components.tsx
@@ -0,0 +1,9 @@
+import { component$ } from '@builder.io/qwik'
+
+export const Signature = component$(() => {
+	return (
+		<div class='flex flex-col items-center justify-center'>
+			<span class='text-xl font-bold'>~ Cody 🚀</span>
+		</div>
+	)
+})
