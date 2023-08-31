3f18df738a8b6c8ccd71417356cff56e4a911f5b
Cody Brunner <me@codybrunner.com>
2023-08-30 20:52:14 -0500

    chore(root): upgrades to astro v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, sharpImageService } from 'astro/config'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true,
-  },
-  integrations: [react(), tailwind(), sitemap()],
-  server: {
-    open: true,
-    port: 3000,
-  },
-  site: 'https://codybrunner.com',
+	image: {
+		service: sharpImageService(),
+	},
+	integrations: [react(), tailwind(), sitemap()],
+	server: {
+		open: true,
+		port: 3000,
+	},
+	site: 'https://codybrunner.com',
 })
